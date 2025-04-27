33da5688445e8485e2bdffa5188ff480b446d8bd
Isteyak Ansari <isteyak07@gmail.com>
2025-04-27 11:11:25 +0530

    adding better console log for understading

--- index.js
+++ index.js
@@ -57,9 +57,9 @@ app.post("/webhook", async (req, res) => {
         req.body.entry[0].changes[0].value.metadata.phone_number_id;
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
       let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-      console.log(phone_number_id);
-      console.log(from);
-      console.log(msg_body);
+      console.log(`here phone number id ${phone_number_id}`);
+      console.log(`here from ${from}`);
+      console.log(`here msg body ${msg_body}`);
     }
   }
 
